37b3aa7d88d184beea3b72d5b47e9d77f9bea2a4
Tonami S <dounanguan@gmail.com>
2025-01-08 09:54:38 +0900

    Add a command

--- test/test.bash
+++ test/test.bash
@@ -2,12 +2,11 @@
 # SPDX-FileCopyrightText: 2024 Tonami Seki
 # SPDX-License-Identifier: BSD-3-Clause
 
-dir=~
-[ "$1" != "" ] && dir="$1"
+dir=~/ros2_ws 
 
-cd $dir/ros2_ws || exit 1
+cd $dir || exit 1
 colcon build || exit 1
-source $dir/ros2_ws/install/setup.bash
+source /opt/ros/humble/setup.bash
 timeout 10 ros2 launch mypkg humidity_monitoring.launch.py > /tmp/mypkg.log 2>&1
 if grep -q 'Received humidity' /tmp/mypkg.log; then
   echo "Test passed!"

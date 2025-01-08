bdc1ce76dbc07ae9e31e9f4356cb3066088c1d73
Tonami S <dounanguan@gmail.com>
2025-01-08 12:13:28 +0900

    Add a command

--- test/test.bash
+++ test/test.bash
@@ -11,17 +11,10 @@ source ~/ros2_ws/install/local_setup.bash
 dir=~
 [ "$1" != "" ] && dir="$1"
 
-cd $dir/ros2_ws
+cd ~/ros2_ws
 colcon build
-if [ $? -ne 0 ]; then
-    echo "Build failed"
-    exit 1
-fi
-source /opt/ros/jazzy/setup.bash
-source ~/ros2_ws/install/local_setup.bash
+
 source install/setup.bash
-source ~/.bashrc
-source ~/ros2_ws/install/local_setup.bash
 
 timeout 10 ros2 launch mypkg humidity_monitoring.launch.py > /tmp/mypkg.log 2>&1
 if grep -q 'Received humidity' /tmp/mypkg.log; then

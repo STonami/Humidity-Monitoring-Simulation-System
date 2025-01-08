033590dc457f8647379293a29c430eacd3303c50
Tonami S <dounanguan@gmail.com>
2025-01-08 11:26:32 +0900

    Add a command

--- test/test.bash
+++ test/test.bash
@@ -6,6 +6,7 @@ source /opt/ros/jazzy/setup.bash
 source ~/ros2_ws/install/setup.bash
 source ~/ros2_ws/install/local_setup.bash
 source ~/.bashrc
+source ~/ros2_ws/install/local_setup.bash
 
 dir=~
 [ "$1" != "" ] && dir="$1"
@@ -20,6 +21,7 @@ source /opt/ros/jazzy/setup.bash
 source ~/ros2_ws/install/local_setup.bash
 source install/setup.bash
 source ~/.bashrc
+source ~/ros2_ws/install/local_setup.bash
 
 timeout 10 ros2 launch mypkg humidity_monitoring.launch.py > /tmp/mypkg.log 2>&1
 if grep -q 'Received humidity' /tmp/mypkg.log; then
